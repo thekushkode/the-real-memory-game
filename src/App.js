022bb25010587ff3843d7ae393023bc17bdce5d6
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-06-10 16:05:19 -0400

    almost working!

--- src/App.js
+++ src/App.js
@@ -1,62 +1,118 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card';
+// import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+//import * as bs from 'react-bootstrap'; shouldnt really do this
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>GWD Presents: Memory</h1>
-        <h4>Match Cards To Win!</h4>
-      </header>
-      <main>
-        <div className="Card-container">
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-        </div>
-        <div className="Card-container">
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-        </div>
-        <div className="Card-container">
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-        </div>
-        <div className="Card-container">
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-        </div>
-      </main>
-    </div>
-  );
+function generateDeck() {
+  let symbols = ['🇺🇸', '🦠', '🖥', '🐐', '🦄', '💩', '😈', '🍆'];
+  let deck = [];
+  for (let i = 0; i < 16; i++) {
+    deck.push({ isFlipped: false, symbol: symbols[i % 8] });
+  }
+  return shuffle(deck);
 };
 
-// let projects = [
-//   { name: 'Project Ish', likes: 1460 },
-//   { name: 'The Kush Kode', likes: 32222 },
-//   { name: 'Goat Web Design', likes: 18700 },
-//   { name: 'BubbaKushner.com', likes: 3890000000 },
-//   { name: 'The Real Facebook', likes: 7238 },
-//   { name: 'Forrest Gump 2: The Return of Bubba', likes: 23451123 },
-// ];
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+};
 
-{/* return (
-  <div>
-  {projects.map((project, index) => {
-    return <Card
-      name={project.name}
-      key={index}
-      likes={project.likes}></Card>
-  })}
-</div>) */}
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: [],
+      won: false,
+    }
+  }
+
+  pickCard = (cardIndex) => {
+    if (this.state.deck[cardIndex].isFlipped) {
+      return;
+    } else {
+      let cardToFlip = { ...this.state.deck[cardIndex] };
+      cardToFlip.isFlipped = true;
+      let newPickedCards = this.state.pickedCards.concat(cardIndex);
+      let newDeck = this.state.deck.map((card, index) => {
+        if (cardIndex === index) {
+          return cardToFlip;
+        }
+        return card;
+      })
+      if (newPickedCards.length === 2) {
+        let card1Index = newPickedCards[0];
+        let card2Index = newPickedCards[1];
+        if (card1Index !== card2Index) {
+          // this.unflipCards(card1Index, card2Index);
+          setTimeout(() => {this.unflipCards(card1Index, card2Index)}, 1000)
+        }
+        newPickedCards = [];
+      }
+      this.setState({
+        deck: newDeck,
+        pickedCards: newPickedCards,
+      })
+    }
+  };
+
+  unflipCards = (card1Index, card2Index) => {
+    let card1 = { ...this.state.deck[card1Index] };
+    let card2 = { ...this.state.deck[card2Index] };
+    card1.isFlipped = false;
+    card2.isFlipped = false;
+    let newDeck = this.state.deck.map((card, index) => {
+      if (card1Index === index) {
+        return card1;
+      }
+      if (card2Index === index) {
+        return card2;
+      }
+      return card;
+    })
+    this.setState({
+      deck: newDeck,
+    })
+  }
+
+
+  render() {
+
+    let cardsJSX = this.state.deck.map((card, index) => {
+      return <Card symbol={card.symbol} isFlipped={card.isFlipped} key={index} pickCard={this.pickCard.bind(this, index)} />
+    })
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>GWD Presents: Memory</h1>
+          <h4>Match Cards To Win!</h4>
+        </header>
+        <main>
+          <div className="Card-container">
+            {cardsJSX.slice(0, 4)}
+          </div>
+          <div className="Card-container">
+            {cardsJSX.slice(4, 8)}
+          </div>
+          <div className="Card-container">
+            {cardsJSX.slice(8, 12)}
+          </div>
+          <div className="Card-container">
+            {cardsJSX.slice(12, 16)}
+          </div>
+        </main>
+      </div>
+    );
+  };
+};
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 import './Card.css';
 //import numeral from 'numeral';
 import goat from '../goat.gif';
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
 
 class Card extends Component {
     // static propTypes = {
     //     name: this.propTypes.string,
     //     likes: this.propTypes.number,
     // }
-    constructor(props) {
-        super(props);
-        this.state = {
-            enabled: false,
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         isFlipped: false,
+    //     }
+    // }
 
-    handleClick = (e) => {
-        this.setState({ enabled: !this.state.enabled });
-    }
+    // handleClick = (e) => {
+    //     this.setState({ isFlipped: !this.state.isFlipped });
+    // }
 
 
     render() {
         let innerClass = "Memory-inner";
-        if (this.state.enabled) {
+        if (this.props.isFlipped) {
             innerClass += " Flipped"
         }
         return (
-            <div className="Memorycard" onClick={this.handleClick}>
+            <div className="Memorycard" onClick={this.props.pickCard}>
                 <div className={innerClass}>
                     <div className="Card-back">
                         <img src={goat} alt="Memory Card with goat on it" className="Goat-img"></img>
                     </div>
                     <div className="Card-front">
-                        ∆
+                        {this.props.symbol}
                     </div>
                 </div>
             </div>
